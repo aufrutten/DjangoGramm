11f435c891a887cf41ad123db85701ae80cc734b
Semykopenko Ihor <aufrutten@gmail.com>
2023-03-15 17:03:14 +0100

    refresh svg in html, including by webpack

--- root/instagram/static/gramm/js/loader_content.js
+++ root/instagram/static/gramm/js/loader_content.js
@@ -11,13 +11,9 @@ function get_status_like(post_id) {
     let url = `/api/v1/profiles/${request_user_id}/likes/${post_id}/`;
     let button_like = `#like_button_${post_id}`
 
-    const hearth = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 -0.5 16 16">' +
-        '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>' +
-        '</svg>'
+    const hearth = '<i width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 -0.5 16 16"></i>'
 
-    const hearth_fill = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">' +
-        '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>' +
-        '</svg>'
+    const hearth_fill = '<i width="30" height="30" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16"></i>'
 
     $.get(url).done(function () {
         $(button_like).html(hearth_fill).css('color', 'red');
@@ -79,11 +75,9 @@ function getHTML(post, email) {
                         </button>
                         <script>get_count_likes(${post.id})</script>
                         <script>get_status_like(${post.id})</script>
-                        <button class="btn" type="button" onclick="location.href='/posts/${post.id}/comments'">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" fill="currentColor" class="bi bi-chat" viewBox="0 1.1 16 16">
-                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                            </svg>
-                        </button>
+                        <a class="btn" onclick="/posts/${post.id}/comments/">
+                            <i width="31" height="31" fill="currentColor" class="bi bi-chat" viewBox="0 1.1 16 16"></i>
+                        </a>
                     </div>
                 </div>
             </div>`
