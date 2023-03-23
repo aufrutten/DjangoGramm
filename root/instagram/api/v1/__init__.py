04d12e6cf3b0963d3330e35535fcf7aeb9b66ae9
Semykopenko Ihor <aufrutten@gmail.com>
2023-03-23 17:35:45 +0100

    update serializer_classes in API V1

--- root/instagram/api/v1/__init__.py
+++ root/instagram/api/v1/__init__.py
@@ -8,15 +8,9 @@ from . import posts
 router = routers.DefaultRouter()
 
 # <-----PROFILES----------------->
-router.register(r'profiles', profile.Profiles, basename='profiles')
-router.register(r'profiles/(?P<profile>[^/.]+)/comments', profile.Comments, basename='profile_comments')
 router.register(r'profiles/(?P<profile>[^/.]+)/likes', profile.Likes, basename='profile_likes')
-router.register(r'profiles/(?P<profile>[^/.]+)/posts', profile.Posts, basename='profile_posts')
 router.register(r'profiles/(?P<profile>[^/.]+)/subscriptions', profile.Subscriptions, basename='profile_subscriptions')
 router.register(r'profiles/(?P<profile>[^/.]+)/subscribers', profile.Subscribers, basename='profile_subscribers')
 
 # <-----POSTS-------------------->
-router.register(r'posts', posts.Posts, basename='posts')
-router.register(r'posts/(?P<post>[^/.]+)/comments', posts.Comments, basename='post_comments')
 router.register(r'posts/(?P<post>[^/.]+)/likes', posts.Likes, basename='post_likes')
-router.register(r'posts/(?P<post>[^/.]+)/profiles', posts.Profile, basename='post_profiles')
--- root/instagram/api/v1/posts.py
+++ root/instagram/api/v1/posts.py
@@ -1,5 +1,5 @@
 
-__all__ = ('Profiles', 'Comments', 'Likes')
+__all__ = ('Likes',)
 
 from rest_framework import serializers
 from rest_framework import viewsets
@@ -14,76 +14,20 @@ from instagram import tools
 from . import custom_permissions
 
 
-class Posts(viewsets.ModelViewSet):
-
-    queryset = Post.objects.all().order_by('id').reverse()
-
-    def get_permissions(self):
-        if str(self.request.method) in ("GET", "HEAD", "POST"):
-            return [permissions.IsAuthenticated()]
-        return [permissions.IsAdminUser()]
-
-    def get_serializer_class(self):
-        class serializer_class(serializers.ModelSerializer):
-
-            image = serializers.ImageField(required=True, allow_null=False)
-
-            if self.request.method == 'POST':
-                tags = serializers.CharField(required=False, allow_blank=False, default='')
-
-            class Meta:
-                model = Post
-                fields = ["id", "user", "image", "tags"]
-                extra_kwargs = {'user': {'read_only': True}}
-
-            def create(self, validated_data):
-                user = get_object_or_404(User, username=self.context.get('request').user)
-                post = tools.add_new_post(user, **validated_data)
-                return post
-
-        return serializer_class
-
-
-class Comments(viewsets.ModelViewSet):
-
-    def get_permissions(self):
-        if str(self.request.method) in ("GET", "HEAD", "POST"):
-            return [permissions.IsAuthenticated()]
-        return [permissions.IsAdminUser()]
-
-    def get_serializer_class(self):
-        class serializer_class(serializers.ModelSerializer):
-            class Meta:
-                model = Comment
-                fields = ["id", "user", "comment"]
-                extra_kwargs = {'user': {'read_only': True}}
-
-            def create(self, validated_data):
-                user = get_object_or_404(User, username=self.context.get('request').user)
-                post = get_object_or_404(Post, id=self.context.get('request').parser_context.get('kwargs')['post'])
-                comment = Comment(user=user, post=post, comment=validated_data['comment'])
-                comment.save()
-                return comment
-
-        return serializer_class
-
-    def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs['post'])
-        return Comment.objects.filter(post=post).all().order_by('id').reverse()
+class LikesSerializer_class(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ["user"]
+        extra_kwargs = {'user': {'read_only': True}}
 
 
 class Likes(viewsets.ModelViewSet):
+    serializer_class = LikesSerializer_class
 
     def get_permissions(self):
         if str(self.request.method) in ("GET", "HEAD", "POST"):
             return [permissions.IsAuthenticated()]
         return [permissions.IsAdminUser()]
-
-    class serializer_class(serializers.ModelSerializer):
-        class Meta:
-            model = Like
-            fields = ["user"]
-            extra_kwargs = {'user': {'read_only': True}}
 
     def create(self, request, *args, **kwargs):
         user = get_object_or_404(User, username=request.user)
@@ -100,19 +44,3 @@ class Likes(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=self.kwargs['post'])
         return Like.objects.filter(post=post).all().order_by('user')
 
-
-class Profile(viewsets.ModelViewSet):
-
-    def get_permissions(self):
-        if str(self.request.method) in ("GET", "HEAD"):
-            return [permissions.IsAuthenticated()]
-        return [permissions.IsAdminUser()]
-
-    class serializer_class(serializers.ModelSerializer):
-        class Meta:
-            model = User
-            fields = ["id", "email", "first_name", "last_name", "birthday"]
-
-    def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs['post'])
-        return User.objects.filter(id=post.user.id).all()
